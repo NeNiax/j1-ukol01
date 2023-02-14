49a05afb905f1066412f749c37fb9a518f3c117b
Neniax <peti69501@gmail.com>
2023-02-14 22:05:36 +0100

    Část 1 - Nakresli prasátko.

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -8,6 +8,42 @@ public class HlavniProgram {
         Turtle zofka = new Turtle();
 
         //TODO implementace domácího úkolu
+
+        //CAST 1 - nakresli prasátko
+        nakresliPrasatko(zofka);
+
+    }
+
+    private static void nakresliPrasatko(Turtle zofka) {
+        // tělo prasátka
+        zofka.turnRight(180);
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+
+        // hlava prasátka
+        zofka.turnRight(45);
+        zofka.move(70);
+        zofka.turnLeft(90);
+        zofka.move(70);
+
+        // nohy prasátka
+        nozicky(zofka);
+        zofka.turnRight(30);
+        zofka.move(100);
+        zofka.turnRight(30);
+        nozicky(zofka);
+    }
+
+    private static void nozicky(Turtle zofka) {
+        zofka.move(25);
+        zofka.turnRight(180);
+        zofka.move(25);
+        zofka.turnLeft(75);
+        zofka.move(25);
+        zofka.turnLeft(180);
+        zofka.move(25);
     }
 
     public static void main(String[] args) {
